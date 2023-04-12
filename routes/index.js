e555cd771d467acc7fb5ea421d444165b6861f3a
sanjay1904m1 <95368219+sanjay1904m1@users.noreply.github.com>
2023-04-12 16:45:29 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+console.log("router reloaded");
+
+router.use("/", require("./users"));
+
+
+
+
+  
+  
+module.exports = router;
